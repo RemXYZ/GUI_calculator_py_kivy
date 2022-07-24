b903878aec893dc6611fd02f0a9ed251c2f2ed83
RemXYZ <grednep@gmail.com>
2022-07-24 17:01:57 +0200

    v0_1

---
+++ main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.widget import GridLayout
+
+
+# pyinstaller main.py
+# to create .exe file
+
+if __name__ != '__main__':
+    exit()
+
+
+class MainWidget(Widget):
+    pass
+
+
+class TheLabApp(App):
+    pass
+
+
+TheLabApp().run()
