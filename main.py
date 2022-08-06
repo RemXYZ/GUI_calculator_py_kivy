108455380b32724e7a35bbf16ab16d5ca48c9a14
RemXYZ <remkarablin20@gmail.com>
2022-08-06 18:32:32 +0200

    outside_layout

--- main.py
+++ main.py
@@ -16,37 +16,6 @@ if __name__ != '__main__':
 # START OF PROGRAM
 
 class MyGridLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGridLayout, self).__init__(**kwargs)
-
-        # Set columns
-        self.cols = 1
-
-        #Second gridlayout
-        self.top_grid = GridLayout()
-        self.top_grid.cols = 2
-
-        #Add widgets
-        self.top_grid.add_widget(Label(text="Name: "))
-
-        self.name = TextInput(multiline = False)
-        self.top_grid.add_widget(self.name)
-
-
-        self.top_grid.add_widget(Label(text="Favorite Pizza: "))
-
-        self.pizza = TextInput(multiline=True)
-        self.top_grid.add_widget(self.pizza)
-
-        # The new top_grid to our app
-        self.add_widget(self.top_grid)
-
-        # submit button
-        self.submit = Button(text="Submit", font_size=32)
-        # bind the button
-        self.submit.bind(on_press=self.press)
-        self.add_widget(self.submit)
-        
 
     def press(self, instance):
         name = self.name.text
