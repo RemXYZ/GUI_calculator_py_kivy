9ef0e707c86ff047144f241af8440ca6464c4b22
RemXYZ <remkarablin20@gmail.com>
2022-08-15 23:27:03 +0200

    working alculator

--- main.py
+++ main.py
@@ -34,13 +34,59 @@ class MyLayout(Widget):
         #update the label
         self.ids.name_label.text = name
 
-    def button_num_press(self, num):
+    def write_num(self, num):
+        if num == ".":
+            return
+
         prior = self.ids.calc_input.text
+        if "Error" in prior:
+            prior = ''
+
         if prior == "0":
             self.ids.calc_input.text = num
         else:
             self.ids.calc_input.text = prior + num
-        print(num)
+
+    def clear(self):
+        self.ids.calc_input.text = '0'
+
+    def remove(self):
+        prior = self.ids.calc_input.text
+        prior = prior[:-1]
+        self.ids.calc_input.text = prior
+
+    def dot(self):
+        prior = self.ids.calc_input.text
+        num_list = prior.split("+")
+
+        if "+" in prior and "." not in num_list[-1]:
+            prior = prior + "."
+            self.ids.calc_input.text = prior
+        elif "." in prior:
+            pass
+        else:
+            prior = prior + "."
+
+        self.ids.calc_input.text = prior
+
+    def pos_neg(self):
+        prior = self.ids.calc_input.text
+        if "-" in prior:
+            self.ids.calc_input.text = f'{prior.replace("-","+")}'
+        else:
+            self.ids.calc_input.text = f'-{prior}'
+
+    def math_sign(self, sign):
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}{sign}'
+
+    def equals(self):
+        prior = self.ids.calc_input.text
+        try:
+            answer = eval(prior)
+            self.ids.calc_input.text = str(answer)
+        except:
+            self.ids.calc_input.text = "Error"
 
 
 class CalculatorApp(App):
